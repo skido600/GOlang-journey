6e53a56ce4e8ba0b7727995b5f2a0a6b91439ddb
Chuks <63956312+skido600@users.noreply.github.com>
2025-07-02 14:54:30 +0200

    first commit

---
+++ hello_world/main.go
@@ -0,0 +1,15 @@
+// package declaration
+package main
+
+// import statement to import the format package necessary for IO operation like scanf ,printf,println
+import "fmt"
+
+// Single-line comment — just like JS
+
+/*
+   Multi-line comment — also like JS
+*/
+
+func main(){
+fmt.Print("hello")
+}
